41a10c64a67b0ab8861c0ad383995b3474c46bc2
leo <lhjang@mybroccoli.co.kr>
2021-02-17 00:29:43 +0900

    cleanup: 불필요 의존성 제거

--- leo-api/build.gradle.kts
+++ leo-api/build.gradle.kts
@@ -6,18 +6,6 @@ dependencies {
 
     implementation(project(":leo-common"))
 
-    //swagger
-    implementation("io.springfox:springfox-swagger2:${DependencyVersions.SWAGGER_VERSION}")
-    implementation("io.springfox:springfox-swagger-ui:${DependencyVersions.SWAGGER_VERSION}")
-
-    // cache
-    implementation("org.ehcache:ehcache:3.8.1")
-    implementation("org.springframework.boot:spring-boot-starter-cache:2.2.6.RELEASE")
-    implementation("javax.cache:cache-api:1.1.1")
-
-    // jsoup
-    implementation("org.jsoup:jsoup:1.13.1")
-
     // jackson
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
