483e5c850b7d0745a830b281dcc5bfedfc4a95c1
leo <lhjang@mybroccoli.co.kr>
2021-02-24 17:31:59 +0900

    feat: JPA Audit

---
+++ leo-common/src/main/kotlin/com/gowid/leo/common/config/AppConfig.kt
@@ -0,0 +1,9 @@
+package com.gowid.leo.common.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@EnableJpaAuditing
+@Configuration
+class AppConfig {
+}
