f13b11c279627f67b15bbc50f9d9bc1402be347e
leo <lhjang@mybroccoli.co.kr>
2021-03-01 00:14:55 +0900

    feat: update api

--- leo-api/src/main/kotlin/com/gowid/leo/api/service/posts/PostsService.kt
+++ leo-api/src/main/kotlin/com/gowid/leo/api/service/posts/PostsService.kt
@@ -1,12 +1,37 @@
 package com.gowid.leo.api.service.posts
 
+import com.gowid.leo.api.web.dto.PostsResponseDto
 import com.gowid.leo.api.web.dto.PostsSaveRequestDto
+import com.gowid.leo.api.web.dto.PostsUpdateRequestDto
 import com.gowid.leo.common.domain.posts.PostsRepository
+import org.springframework.data.jpa.domain.AbstractPersistable_.id
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class PostsService(private val postsRepository: PostsRepository) {
 
+    @Transactional
     fun save(requestDto: PostsSaveRequestDto): Long? = postsRepository.save(requestDto.toEntity()).id
+
+    @Transactional
+    fun update(id: Long, requestDto: PostsUpdateRequestDto): Long? {
+        val posts = postsRepository.findByIdOrNull(id)
+                ?: throw IllegalArgumentException("해당 게시글이 없습니다. id=$id")
+
+        posts.update(requestDto.title, requestDto.content)
+
+        return posts.id
+    }
+
+    @Transactional(readOnly = true)
+    fun findById(id: Long): PostsResponseDto {
+        val entity = postsRepository.findByIdOrNull(id)
+                ?: throw IllegalArgumentException("해당 게시글이 없습니다. id=$id")
+
+        return PostsResponseDto(entity)
+    }
+
 
 }
--- leo-api/src/main/kotlin/com/gowid/leo/api/web/controller/PostsApiController.kt
+++ leo-api/src/main/kotlin/com/gowid/leo/api/web/controller/PostsApiController.kt
@@ -1,18 +1,24 @@
 package com.gowid.leo.api.web.controller
 
 import com.gowid.leo.api.service.posts.PostsService
+import com.gowid.leo.api.web.dto.PostsResponseDto
 import com.gowid.leo.api.web.dto.PostsSaveRequestDto
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import com.gowid.leo.api.web.dto.PostsUpdateRequestDto
+import org.springframework.web.bind.annotation.*
 import java.time.LocalDateTime
 
 @RestController
 class PostsApiController(private val postsService: PostsService) {
 
     @PostMapping("/api/v1/posts")
-    fun save(@RequestBody requestDto : PostsSaveRequestDto) : Long? = postsService.save(requestDto)
+    fun save(@RequestBody requestDto : PostsSaveRequestDto): Long? = postsService.save(requestDto)
+
+    @PutMapping("/api/v1/posts/{id}")
+    fun update(@PathVariable id: Long,
+               @RequestBody requestDto: PostsUpdateRequestDto): Long? = postsService.update(id, requestDto)
+
+    @GetMapping("/api/v1/posts/{id}")
+    fun findById(@PathVariable id: Long): PostsResponseDto? = postsService.findById(id)
 
     @GetMapping("/health-check")
     fun healthCheck() : String? = LocalDateTime.now().toString()
